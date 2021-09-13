8bfbbf4c5e58e2f002f517df8deed8bf1ba8effd
Jens Petersen <petersen@redhat.com>
2021-09-14 01:13:34 +0800

    create: always offer to pull latest image (fixes: #873)

--- src/cmd/create.go
+++ src/cmd/create.go
@@ -686,10 +686,6 @@ func pullImage(image, release string) (bool, error) {
 
 	logrus.Debugf("Looking for image %s", imageFull)
 
-	if _, err := podman.ImageExists(imageFull); err == nil {
-		return true, nil
-	}
-
 	domain := utils.ImageReferenceGetDomain(imageFull)
 	if domain == "" {
 		panicMsg := fmt.Sprintf("failed to get domain from %s", imageFull)
@@ -705,7 +701,9 @@ func pullImage(image, release string) (bool, error) {
 	}
 
 	if promptForDownload {
-		fmt.Println("Image required to create toolbox container.")
+		if _, err := podman.ImageExists(imageFull); err != nil {
+			fmt.Println("Image required to create toolbox container.")
+		}
 
 		prompt := fmt.Sprintf("Download %s (500MB)? [y/N]:", imageFull)
 		shouldPullImage = askForConfirmation(prompt)
